a025da5f99a7da84bf51ac390387db236bc1cecc
arzaga <alexis.0529@hotmail.com>
2017-09-10 13:48:59 -0600

    Initial commit

---
+++ app/src/main/java/com/example/alexi/eva1_2_fibonacci_ordenmenor_serie/Principal.java
@@ -0,0 +1,13 @@
+package com.example.alexi.eva1_2_fibonacci_ordenmenor_serie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
